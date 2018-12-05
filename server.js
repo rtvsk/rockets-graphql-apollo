5afe95bb7e9a3d9862df7f0c05dbb97e04fc0630
Dmitry Ivanov <Webdev.Dmitry@gmail.com>
2018-12-06 02:04:29 +0300

    add schema and server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema');
+
+const app = express();
+
+// One endpoint graphql
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Сервер запущен, порт ${PORT}`));
