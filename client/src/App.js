7ed16e3df13ab52c68474b4d76e5479e7ad548fb
Dmitry Ivanov <Webdev.Dmitry@gmail.com>
2018-12-06 22:42:27 +0300

    add front-end

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Launches from './components/Launches';
+import Launch from './components/Launch';
+
+import './App.css';
+import logo from './spacex-logo.png';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql'
+})
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <Router>
+          <div className="container">
+            <img 
+              src={logo}
+              alt="SpaceX"
+              style={{width: '280px', display: 'block', margin: 'auto'}}
+            />
+            <Route exact path="/" component={Launches} />
+            <Route path="/launch/:flight_number" component={Launch} />
+          </div>
+        </Router>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
